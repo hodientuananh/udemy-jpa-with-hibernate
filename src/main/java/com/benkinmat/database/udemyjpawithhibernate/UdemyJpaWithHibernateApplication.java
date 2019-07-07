b6f90d81322bfdae9121468484b3af306f1c8f50
Tuan Anh Ho Dien <benkinmat@gmail.com>
2019-07-07 14:55:58 +0700

    initial spring boot

---
+++ src/main/java/com/benkinmat/database/udemyjpawithhibernate/UdemyJpaWithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.benkinmat.database.udemyjpawithhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJpaWithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyJpaWithHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benkinmat/database/udemyjpawithhibernate/UdemyJpaWithHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.benkinmat.database.udemyjpawithhibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyJpaWithHibernateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
