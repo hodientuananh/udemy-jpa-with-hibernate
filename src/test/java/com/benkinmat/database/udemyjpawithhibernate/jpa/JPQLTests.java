4a26c2cd3749e3ebe7796c606aed0652cf574087
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-07-08 13:58:45 +0700

    jpql test

---
+++ src/test/java/com/benkinmat/database/udemyjpawithhibernate/jpa/JPQLTests.java
@@ -0,0 +1,43 @@
+package com.benkinmat.database.udemyjpawithhibernate.jpa;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.benkinmat.database.udemyjpawithhibernate.UdemyJpaWithHibernateApplication;
+import com.benkinmat.database.udemyjpawithhibernate.entity.Course;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UdemyJpaWithHibernateApplication.class)
+public class JPQLTests {
+
+	private static final Logger log = LoggerFactory.getLogger(UdemyJpaWithHibernateApplication.class);
+	
+	@Autowired
+	EntityManager entityManager;
+	
+	@Test
+	public void selectAll() {
+		List<Course>courses = entityManager.createQuery("select c from Course c", Course.class).getResultList();
+		log.info("courses: " + courses);
+	}
+	
+	@Test
+	public void selectAllWithWhere() {
+		List<Course>courses = entityManager.createQuery("select c from Course c where c.name like '%Learn new%'", Course.class).getResultList();
+		log.info("courses: " + courses);
+	}
+	
+}
